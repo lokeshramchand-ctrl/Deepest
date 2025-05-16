02bc1f57af97118d6a97092d37f3a796b0bd4077
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-05-16 15:16:13 +0530

    Armstrong

---
+++ Armstrong.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Armstrong {
+      public static void main(String[] args) {
+            int n;
+            try (Scanner sc = new Scanner(System.in)) {
+                  n = sc.nextInt();
+            }
+            int original = n;
+            int remainder = 0;
+            int count = 0;
+            while(n > 0)
+            {
+                  int digit = n % 10;
+                  remainder = remainder + (digit * digit * digit);
+                  n = n / 10;
+                  count++;
+            }
+            if(remainder == original)
+            {
+                  System.out.println("The num is armstrong");
+            }
+            else
+            {
+                  System.out.println("Not an Armstrong number");
+            }
+      }
+}
--- SumDigits.java
+++ SumDigits.java
@@ -6,13 +6,13 @@ public class SumDigits {
             try (Scanner sc = new Scanner(System.in)) {
                   n = sc.nextInt();
             }
+            int remainder = 0;
             while(n > 0)
             {
                   int digit = n % 10;
+                   remainder = remainder + digit;
                   n = n / 10;
-                  System.out.println(digit+n);
-
-
             }
+            System.out.println(remainder);
       }
 }

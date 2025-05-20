77156afac58e977b4cec3969acc5de38d6e05c59
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-05-20 19:34:20 +0530

    ANother

---
+++ CheckSorted.java
@@ -0,0 +1,66 @@
+
+import java.util.Scanner;
+
+public class CheckSorted {
+      public static void main(String[] args)
+      {
+            int n;
+            int so = 0;
+            try (Scanner sc = new Scanner(System.in)) {
+                  n = sc.nextInt();
+                  int arr[] = new int[n];
+                  for(int i = 0 ; i < n ; i++)
+                  {
+                        arr[i] = sc.nextInt();
+                  }
+                  for(int i = 0 ; i <= n-2 ; i++)
+                  {
+                        if(arr[i] <= arr[i+1])
+                        {
+                              so++;
+                        }
+                        else
+                        {
+                              System.out.println("Not Sorted");
+                                                            break;
+
+                        }
+                  }
+            }
+
+      }
+}
+
+
+
+/*import java.util.Scanner;
+
+public class CheckSorted {
+    public static void main(String[] args) {
+        int n;
+        boolean isSorted = true;
+
+        try (Scanner sc = new Scanner(System.in)) {
+            n = sc.nextInt();
+            int arr[] = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            for (int i = 0; i < n - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    isSorted = false;
+                    break;
+                }
+            }
+
+            if (isSorted) {
+                System.out.println("Sorted");
+            } else {
+                System.out.println("Not Sorted");
+            }
+        }
+    }
+}
+ */

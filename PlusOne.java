719954a9edd98765eb1257c2de5a95dccbbce940
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-05-26 15:29:55 +0530

    26-05-2025

--- PlusOne.java
+++
@@ -1,9 +0,0 @@
-public class PlusOne {
-
-      public static void main(String[] args) {
-            int[] input = { 1, 2, 3 }; 
-            
-            System.out.println(input[2]);
-
-      }
-}

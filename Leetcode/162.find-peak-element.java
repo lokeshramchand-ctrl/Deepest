3e9fe38749d2c773d1b366f92a89e767f3a5837c
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-07-14 16:08:06 +0530

    Peak Element

--- Leetcode/162.find-peak-element.java
+++ Leetcode/162.find-peak-element.java
@@ -56,8 +56,28 @@
 // @lc code=start
 class Solution {
     public int findPeakElement(int[] nums) {
-        
+        int n = nums.length;
+
+        // Base case: single element
+        if (n == 1) return 0;
+
+        // Check if the first element is a peak
+        if (nums[0] > nums[1]) return 0;
+
+        // Check middle elements
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+                return i;
+            }
+        }
+
+        // Check if the last element is a peak
+        if (nums[n - 1] > nums[n - 2]) return n - 1;
+
+        // Default case (though problem guarantees a peak exists)
+        return -1;
     }
 }
+
 // @lc code=end
 

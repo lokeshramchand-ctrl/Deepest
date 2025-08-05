1d699589e81e3255f11df02718b55c662e6f52fa
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-08-05 19:13:34 +0530

    Leetcode

--- 2161.partition-array-according-to-given-pivot.java
+++
@@ -1,7 +0,0 @@
-git class Solution {
-    public int[] pivotArray(int[] nums, int pivot) {
-        
-    }
-}
-// @lc code=end
-
---
+++ Leetcode/238.product-of-array-except-self.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=238 lang=java
+ *
+ * [238] Product of Array Except Self
+ */
+
+// @lc code=start
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int index = 0;
+        int[] ans = new int[nums.length];
+        for(int i = 0 ; i < nums.length; i++)
+        {
+            
+        }
+    }
+}
+// @lc code=end
+

f627f0833062743755cbacacf014bf6a5e7b896a
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-08-19 19:14:16 +0530

    two:

---
+++ 128.longest-consecutive-sequence.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=128 lang=java
+ *
+ * [128] Longest Consecutive Sequence
+ */
+
+// @lc code=start
+
+import java.util.Arrays;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Arrays.sort(nums);
+        return 0;
+        
+    }
+}
+// @lc code=end
+

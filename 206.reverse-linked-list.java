b6909c8870711ee62e1553a187c1b8d29eb88629
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-08-26 22:00:02 +0530

    RE

--- 206.reverse-linked-list.java
+++ 206.reverse-linked-list.java
@@ -17,7 +17,17 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        
+        ListNode curr = head;
+        ListNode prev= null;
+        while(curr!=null)
+        {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+
+        }
+        return prev;
     }
 }
 // @lc code=end

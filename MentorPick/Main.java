80b56b19d02a74ac065e4dc42cdb24b7adaf7190
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-09-15 13:34:13 +0530

    oho-34(Reback)

--- MentorPick/Main.java
+++ MentorPick/Main.java
@@ -1,11 +1,21 @@
 class Solution {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         String text = "Hello World";
-        String[] words = text.split("\\s+"); 
-        for(int i = 0 ; i < words.length ; i++)
-        {
+        String[] words = text.split("\\s+");
+        for (int i = 0; i < words.length; i++) {
             System.out.print(words[i] + " ");
         }
+        System.out.println();
+        String check = "ad";
+        char[] charsToCheck = check.toCharArray();
+
+        for (String word : words) {
+            for (char c : charsToCheck) {
+                if (word.indexOf(c) != -1) {
+                    System.out.println(word + " contains the letter " + c);
+                }
+            }
+        }
     }
+
 }

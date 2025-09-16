d113a63640cb3140759ede12cdfcc66486cc2983
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-09-16 15:00:57 +0530

    Comt

--- MentorPick/Main.java
+++ MentorPick/Main.java
@@ -1,31 +1,17 @@
 
 class Solution {
     public static void main(String[] args) {
-        int[][] nums = { { 1, 1, 1 },
-                { 1, 0, 1 },
-                { 1, 1, 1 } };
-        int m = nums.length;
-        int n = nums[0].length;
-        boolean zeroi[] = new boolean[m];
-        boolean zeroesj[] = new boolean[n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (nums[i][j] == 0) {
-                    zeroi[i] = true;
-                    zeroesj[j] = true;
-                }
-            }
-        }
-
-        for (int i = 0; i < m; i++) {
-            for(int j = 0 ; j < n ; j++)
+        int[] nums =  {2,6,4,8,10,9,15};
+        int count = 0;
+        for(int i = 0 ; i < nums.length - 1 ; i++)
+        {
+            if(nums[i + 1] != nums[i] + 1)
             {
-                if(zeroi[i] == true || zeroesj[j] == true)
-                {
-                    nums[i][j] = 0;
-                }
+                count++;
             }
         }
+        System.out.println(count - 1);
+        
         
     }
 }

28b71af6fa4fa57e623e9f3815da29128aac999b
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-09-21 19:32:25 +0530

    re

--- Hehe.java
+++ Hehe.java
@@ -1,18 +1,43 @@
-public class Hehe{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hehe {
     public static void main(String[] args) {
-        String s = "abcd";
-        String t = "abcde";
-        for (int i = 0; i < s.length(); i++) {
-            System.out.print(s.charAt(i) + " ");
+        String s = "ab#c";
+        String t = "ad#c";
+
+        // s
+
+        char[] chars = s.toCharArray();
+        List<Character> charList = new ArrayList<>();
+        for (char c : chars)
+            charList.add(c);
+
+        charList.remove(Character.valueOf('#'));
+
+        char[] result = new char[charList.size()];
+        for (int i = 0; i < charList.size(); i++)
+            result[i] = charList.get(i);
+        System.out.println(result);
+
+        //t
+        char[] chas = s.toCharArray();
+        List<Character> arr = new ArrayList<>();
+        for (char c : chas)
+            arr.add(c);
+
+        charList.remove(Character.valueOf('#'));
+
+        char[] res = new char[charList.size()];
+        for (int i = 0; i < charList.size(); i++)
+            res[i] = charList.get(i);
+        System.out.println(res);
+        // End
+        if (s == t) {
+            System.out.print(true);
+        } else {
+            System.out.println(false);
         }
-        System.out.println();
-        for (int i = 0; i < t.length(); i++) {
-            System.out.print(t.charAt(i) + " ");
-        }
-        if(s.contains(t))
-        {
-            System.out.print(t);
-        }
-        
+
     }
 }
